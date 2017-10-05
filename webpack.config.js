10322a6f248dc6f22f5e1cb73f7817e69ae9dcda
Kyle Conkright <kyleconkright@gmail.com>
2017-10-05 12:16:52 -0700

    added chunks plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,44 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const projectRoot = path.resolve(__dirname, './');
 
 module.exports = {
-    entry: './src/main.ts',
-    output: {
-        filename: 'app.js',
-        path: path.join(__dirname, './dist')
-    },
-    module: {
+	entry: { 
+		app: projectRoot + '/src/main.ts',
+		polyfills: projectRoot + '/src/polyfills.ts'
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, './dist')
+	},
+	module: {
 		rules: [
-			{test: /\.ts$/, use: 'ts-loader'},
-			{test: /\.html$/, use: 'raw-loader'},
-			{test: /\.s?css$/, use: ['raw-loader','sass-loader'], exclude: /node_modules/, include: path.join(__dirname, './src')}
+			{ test: /\.ts$/, use: 'ts-loader' },
+			{ test: /\.html$/, use: 'raw-loader' },
+			{ test: /\.s?css$/, use: ['raw-loader', 'sass-loader'], exclude: /node_modules/, include: path.join(__dirname, './src') }
 		]
 	},
 	resolve: {
 		extensions: ['.js', '.ts', '.html', '.css']
 	},
 	plugins: [
+		new CleanWebpackPlugin(['./dist']),
 		new HtmlWebpackPlugin({
 			template: './src/index.html'
-        }),
-        
-        new webpack.ContextReplacementPlugin(
-            /(.+)?angular(\\|\/)core(.+)?/,
-            path.join(__dirname, 'src'),
-            {}
-		  ),
+		}),
+
+		new webpack.ContextReplacementPlugin(
+			/(.+)?angular(\\|\/)core(.+)?/,
+			path.join(__dirname, 'src'),
+			{}
+		),
+
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['app', 'polyfills']
+		}),
 	],
 	devServer: {
 		contentBase: path.join(__dirname, './dist'),
